9a64159462839361c0ef02b57f16bacd57569fd7
Olha Starykova <Ole4kastar79@gmail.com>
2026-01-09 08:15:32 +0200

    TicketBookingSystem perform

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,34 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TicketBookingSystem {
 
-    public TicketBookingSystem(int totalSeats) {
+    private int totalSeats;
+    private Semaphore semaphore;
+    private Lock lock = new ReentrantLock();
 
+    public TicketBookingSystem(int totalSeats) {
+        this.totalSeats = totalSeats;
+        this.semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        BookingResult bookingResult;
+        if (semaphore.tryAcquire()) {
+            synchronized (lock) {
+                lock.lock();
+                bookingResult = new BookingResult(user, true, "Booking successful.");
+                this.totalSeats--;
+                semaphore.release();
+                semaphore = new Semaphore(totalSeats);
+                lock.unlock();
+            }
+        } else {
+            bookingResult = new BookingResult(user, false, "No seats available.");
+        }
+        return bookingResult;
     }
 }
